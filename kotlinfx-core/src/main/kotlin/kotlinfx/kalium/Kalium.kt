fedd8d5998dee621e78949828dc1058c9ecbb4a3
Eugen Kiss <eugen@eugenkiss.com>
2014-05-03 09:50:24 +0200

    Templatize Kalium

--- kotlinfx-core/src/main/kotlin/kotlinfx/kalium/Kalium.kt
+++ kotlinfx-core/src/main/kotlin/kotlinfx/kalium/Kalium.kt
@@ -1,5 +1,11 @@
 package kotlinfx.kalium
 
+import javafx.beans.value.ObservableValue
+import javafx.beans.value.WritableValue
+import javafx.scene.Node
+import javafx.scene.control.ComboBoxBase
+import javafx.scene.control.TextInputControl
+
 private var enclosing: Pair<Any, String>? = null
 private val calcMap: MutableMap<Pair<Any, String>, () -> Unit> = hashMapOf()
 
@@ -7,76 +13,35 @@ private val calcMap: MutableMap<Pair<Any, String>, () -> Unit> = hashMapOf()
 private var isConstruction = false
 private val listenerMap: MutableMap<Pair<Any, String>, MutableSet<Any>> = hashMapOf()
 
-public fun javafx.scene.Node.disable(f: (() -> Boolean)? = null): Boolean {
+private fun template<T>(name: String, f: (() -> T)?, thiz: Any, property: ObservableValue<T>): T {
     if (f == null) {
         if (enclosing != null &&
-        (isConstruction || !listenerMap.containsKey(enclosing) || !listenerMap.get(enclosing)!!.contains(this))) {
+        (isConstruction || !listenerMap.containsKey(enclosing) || !listenerMap.get(enclosing)!!.contains(thiz))) {
             val e = enclosing!!
-            listenerMap.getOrPut(e) { hashSetOf() } .add(this)
-            disableProperty()!!.addListener { (v: Any?, o: Any?, n: Any?) -> calcMap.get(e)!!() }
+            listenerMap.getOrPut(e) { hashSetOf() } .add(thiz)
+            property.addListener { (v: Any?, o: Any?, n: Any?) -> calcMap.get(e)!!() }
         }
     }
     else {
-        val e = Pair(this, "disable")
-        val g = { enclosing = e; this.setDisable(f()); enclosing = null }
-        calcMap.put(e, g)
-        isConstruction = true; g(); isConstruction = false
-    }
-    return this.isDisabled()
-}
-
-public fun javafx.scene.Node.style(f: (() -> String)? = null): String {
-    if (f == null) {
-        if (enclosing != null &&
-        (isConstruction || !listenerMap.containsKey(enclosing) || !listenerMap.get(enclosing)!!.contains(this))) {
-            val e = enclosing!!
-            listenerMap.getOrPut(e) { hashSetOf() } .add(this)
-            styleProperty()!!.addListener { (v: Any?, o: Any?, n: Any?) -> calcMap.get(e)!!() }
+        if (property is WritableValue<*>) {
+            val property = property as WritableValue<T>
+            val e = Pair(thiz, name)
+            val g = { enclosing = e; property.setValue(f()); enclosing = null }
+            calcMap.put(e, g)
+            isConstruction = true; g(); isConstruction = false
         }
     }
-    else {
-        val e = Pair(this, "style")
-        val g = { enclosing = e; this.setStyle(f()); enclosing = null }
-        calcMap.put(e, g)
-        isConstruction = true; g(); isConstruction = false
-    }
-    return this.getStyle()!!
+    return property.getValue()!!
 }
 
-public fun javafx.scene.control.ComboBoxBase<String>.value(f: (() -> String)? = null): String {
-    if (f == null) {
-        if (enclosing != null &&
-        (isConstruction || !listenerMap.containsKey(enclosing) || !listenerMap.get(enclosing)!!.contains(this))) {
-            val e = enclosing!!
-            listenerMap.getOrPut(e) { hashSetOf() } .add(this)
-            valueProperty()!!.addListener { (v: Any?, o: Any?, n: Any?) -> calcMap.get(e)!!() }
-        }
-    }
-    else {
-        val e = Pair(this, "value")
-        val g = { enclosing = e; this.setValue(f()); enclosing = null }
-        calcMap.put(e, g)
-        isConstruction = true; g(); isConstruction = false
-    }
-    return this.getValue()!!
-}
+public fun Node.disable(f: (() -> Boolean)? = null): Boolean =
+    template<Boolean>("disable", f, this, disableProperty()!!)
 
-public fun javafx.scene.control.TextInputControl.text(f: (() -> String)? = null): String {
-    if (f == null) {
-        if (enclosing != null &&
-        (isConstruction || !listenerMap.containsKey(enclosing) || !listenerMap.get(enclosing)!!.contains(this))) {
-            val e = enclosing!!
-            listenerMap.getOrPut(e) { hashSetOf() } .add(this)
-            textProperty()!!.addListener { (v: Any?, o: Any?, n: Any?) -> calcMap.get(e)!!() }
-        }
-    }
-    else {
-        val e = Pair(this, "text")
-        val g = { enclosing = e; this.setText(f()); enclosing = null }
-        calcMap.put(e, g)
-        isConstruction = true; g(); isConstruction = false
-    }
-    return this.getText()!!
-}
+public fun Node.style(f: (() -> String)? = null): String =
+    template<String>("style", f, this, styleProperty()!!)
 
+public fun ComboBoxBase<String>.value(f: (() -> String)? = null): String =
+    template<String>("value", f, this, valueProperty()!!)
 
+public fun TextInputControl.text(f: (() -> String)? = null): String =
+    template<String>("text", f, this, textProperty()!!)
